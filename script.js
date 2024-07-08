df1d66af14f7fe5ae5a5c7aadb73793275edaeef
NISHALINI <nishalinithirukkumar2@gmail.com>
2024-07-08 19:25:59 +0530

    commit

--- script.js
+++ script.js
@@ -75,3 +75,60 @@ document.addEventListener("DOMContentLoaded", () => {
   // mix it up portfolio section
   
   var mixer = mixitup('.portfolio-gallery');
+
+
+  // active menu////
+
+  let menuli = document.querySelectorAll('header ul li a');
+  let section = document.querySelectorAll('section');
+
+  function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuli.forEach(sec=> sec.classList.remove("active"));
+    menuli[len].classList.add("active");
+  }
+
+  activeMenu();
+  window.addEventListener("scroll",activeMenu);
+
+
+  // sticky navbar
+  const header = document.querySelector("header");
+  window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+  })
+
+  // toggle icon navbar
+  let menuIcon = document.querySelector("#menu-icon");
+  let navList = document.querySelector(".navlist");
+
+  menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navList.classList.toggle("open")
+  }
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navList.classList.remove("open")
+  }
+
+  // parallax///////////////////////
+  const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+      if(entry.isIntersecting){
+        entry.target.classList.add("show-items")
+      }else{
+        entry.target.classList.remove("show-items")
+      }
+    })
+  })
+
+  const scrollScale = document.querySelectorAll(".scroll-scale");
+  scrollScale.forEach((e1)=>observer.observe(e1));
+
+  const scrollBottom = document.querySelectorAll(".scroll-bottom");
+  scrollBottom.forEach((e1)=>observer.observe(e1));
+
+  const scrollTop = document.querySelectorAll(".scroll-top");
+  scrollTop.forEach((e1)=>observer.observe(e1));
